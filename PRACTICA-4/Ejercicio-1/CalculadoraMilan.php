7e0d8420e763bfacf7c6a47a05e64aac7fecb123
unknown <uo269783@uniovi.es>
2022-12-15 15:33:48 +0100

    aaaa

--- PRACTICA-4/Ejercicio-1/CalculadoraMilan.php
+++ PRACTICA-4/Ejercicio-1/CalculadoraMilan.php
@@ -18,6 +18,7 @@
         protected $editable;
         protected $valor;
         protected $memoria;
+        protected $resultado;
 
         public function __construct() {
             $this->anterior = null;
@@ -26,223 +27,224 @@
             $this->valor = "0";
             $this->memoria = Number(0);
     
-            // document.addEventListener('keydown', (event) => this.procesarTeclas(event));
+            // document.addEventListener('keydown', (event) => $this->procesarTeclas(event));
         }
     
+        
         public function mostrarTexto() {
             $texto = document.getElementsByTagName("input")[0];
             texto.value = $this->valor;
         }
     
-        digitos(value) {
+        public function digitos($value) {
     
-            if (this.valor == "0" || this.editable) {
-                this.valor = "" + value;
-                this.editable = false;
+            if ($this->valor == "0" || $this->editable) {
+                $this->valor = "" + $value;
+                $this->editable = false;
             }
             else
-                this.valor += value;
+            $this->valor += $value;
     
-            this.mostrarTexto();
+            $this->mostrarTexto();
         }
     
-        punto() {
+        public function punto() {
     
-            if (!this.valor.includes("."))
-                this.valor += ".";
+            if (!$this->valor.includes("."))
+                $this->valor += ".";
     
-            this.editable = false;
-            this.mostrarTexto();
+            $this->editable = false;
+            $this->mostrarTexto();
         }
     
-        borrarTodo() {
-            this.valor = "0";
-            this.anterior = null;
-            this.editable = true;
-            this.operador = null;
-            this.mostrarTexto();
+        public function borrarTodo() {
+            $this->valor = "0";
+            $this->anterior = null;
+            $this->editable = true;
+            $this->operador = null;
+            $this->mostrarTexto();
         }
     
-        borrar() {
-            this.valor = "0";
-            this.editable = true;
-            this.mostrarTexto();
+        public function borrar() {
+            $this->valor = "0";
+            $this->editable = true;
+            $this->mostrarTexto();
         }
     
-        basica(operador) {
+        public function basica($operador) {
     
             //no hay numero anterior
-            if (this.anterior == null || this.editable) {
-                this.anterior = this.valor;
-                this.valor = "0";
-                this.operador = operador;
+            if ($this->anterior == null || $this->editable) {
+                $this->anterior = $this->valor;
+                $this->valor = "0";
+                $this->operador = $operador;
             }
     
             //hay numero anterior
             else {
-                this.valor = "" + eval(Number(this.anterior) + this.operador + Number(this.valor));
-                this.anterior = this.valor;
-                this.editable = true;
-                this.operador = operador;
+                $this->valor = "" + eval(Number($this->anterior) + $this->operador + Number($this->valor));
+                $this->anterior = $this->valor;
+                $this->editable = true;
+                $this->operador = $operador;
             }
     
-            this.mostrarTexto();
+            $this->mostrarTexto();
         }
     
-        sumar() {
-            this.basica("+");
+        public function sumar() {
+            $this->basica("+");
         }
     
-        restar() {
-            this.basica("-");
+        public function restar() {
+            $this->basica("-");
         }
     
-        multiplicar() {
-            this.basica("*");
+        public function multiplicar() {
+            $this->basica("*");
         }
     
-        dividir() {
-            this.basica("/");
+        public function dividir() {
+            $this->basica("/");
         }
     
-        raiz() {
-            if (Number(this.valor) > 0) {
-                this.valor = "" + Math.sqrt(Number(this.valor));
-                this.editable = true;
+        public function raiz() {
+            if (Number($this->valor) > 0) {
+                $this->valor = "" + Math.sqrt(Number($this->valor));
+                $this->editable = tru0.e;
             }
-            this.mostrarTexto();
+            $this->mostrarTexto();
         }
     
-        igual() {
-            if (this.anterior == null) {
-                this.valor = "" + this.valor;
-            }
-            else {
-                this.valor = "" + eval(Number(this.anterior) + this.operador + Number(this.valor));
-                this.editable = true;
-                this.anterior = null;
-                this.operador = null;
-            }
-    
-            this.mostrarTexto();
-        }
-    
-        porcentaje() {
-            if (this.anterior == null) { //si no hay anterior, simplemente se pone a 0
-                this.valor = "0";
+        public function igual() {
+            if ($this->anterior == null) {
+                $this->valor = "" + $this->valor;
             }
             else {
-                var percent = Number(this.valor);
-                this.valor = "" + eval(Number(this.anterior) * percent / 100);
-                this.editable = true;
+                $this->valor = "" + eval(Number($this->anterior) + $this->operador + Number($this->valor));
+                $this->editable = true;
+                $this->anterior = null;
+                $this->operador = null;
             }
     
-            this.mostrarTexto();
+            $this->mostrarTexto();
         }
     
-        masMenos() {
-            this.valor = "" + eval(Number(this.valor) + "*-1");
-            this.mostrarTexto();
+        public function porcentaje() {
+            if ($this->anterior == null) { //si no hay anterior, simplemente se pone a 0
+                $this->valor = "0";
+            }
+            else {
+                $percent = Number($this->valor);
+                $this->valor = "" + eval(Number($this->anterior) * $percent / 100);
+                $this->editable = true;
+            }
+    
+            $this->mostrarTexto();
         }
     
-        mrc() {
-            this.valor = this.memoria.toString();
-            this.editable = true;
-            this.mostrarTexto();
+        public function masMenos() {
+            $this->valor = "" + eval(Number($this->valor) + "*-1");
+            $this->mostrarTexto();
         }
     
-        mmas() {
-            this.memoria += Number(this.valor);
-            this.editable = true;
+        public function mrc() {
+            $this->valor = $this->memoria.toString();
+            $this->editable = true;
+            $this->mostrarTexto();
         }
     
-        mmenos() {
-            this.memoria -= Number(this.valor);
-            this.editable = true;
+        public function mmas() {
+            $this->memoria += Number($this->valor);
+            $this->editable = true;
         }
     
-        procesarTeclas(event) {
+        public function mmenos() {
+            $this->memoria -= Number($this->valor);
+            $this->editable = true;
+        }
+    
+        /* public function procesarTeclas(event) {
             switch (event.key) {
                 case '1':
-                    this.digitos(1);
+                    $this->digitos(1);
                     break;
                 case '2':
-                    this.digitos(2);
+                    $this->digitos(2);
                     break;
                 case '3':
-                    this.digitos(3);
+                    $this->digitos(3);
                     break;
                 case '4':
-                    this.digitos(4);
+                    $this->digitos(4);
                     break;
                 case '5':
-                    this.digitos(5);
+                    $this->digitos(5);
                     break;
                 case '6':
-                    this.digitos(6);
+                    $this->digitos(6);
                     break;
                 case '7':
-                    this.digitos(7);
+                    $this->digitos(7);
                     break;
                 case '8':
-                    this.digitos(8);
+                    $this->digitos(8);
                     break;
                 case '9':
-                    this.digitos(9);
+                    $this->digitos(9);
                     break;
                 case '0':
-                    this.digitos(0);
+                    $this->digitos(0);
                     break;
                 case '+':
-                    this.sumar();
+                    $this->sumar();
                     break;
                 case '-':
-                    this.restar();
+                    $this->restar();
                     break;
                 case '*':
-                    this.multiplicar();
+                    $this->multiplicar();
                     break;
                 case '/':
-                    this.dividir();
+                    $this->dividir();
                     break;
                 case 'Enter':
                     event.preventDefault();
-                    this.igual();
+                    $this->igual();
                     break;
                 case 'c':
-                    this.borrarTodo();
+                    $this->borrarTodo();
                     break;
                 case 'Backspace':
-                    this.borrar();
+                    $this->borrar();
                     break;
                 case '.':
-                    this.punto();
+                    $this->punto();
                     break;
                 case 'r':
-                    this.raiz();
+                    $this->raiz();
                     break;
                 case 'm':
-                    this.mrc();
+                    $this->mrc();
                     break;
                 case 'n':
-                    this.mmenos();
+                    $this->mmenos();
                     break;
                 case 'b':
-                    this.mmas();
+                    $this->mmas();
                     break;
                 case 's':
-                    this.masMenos();
+                    $this->masMenos();
                     break;
                 case 'p':
-                    this.porcentaje();
+                    $this->porcentaje();
                     break;
             }
     
-        }
+        } */
     
     }
     
-    var calculadora = new Calculadora();
+    $var calculadora = new Calculadora();
     ?>
 </head>
 
